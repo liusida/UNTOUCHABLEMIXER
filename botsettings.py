1a187762b6202251000eed3749eea84c392bc0fc
sida <sliu1@uvm.edu>
2020-02-14 11:21:53 -0500

    create a folder for proj01

--- botsettings.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 14 09:29:32 2020
-
-@author: sarahsmith
-"""
-
-API_TOKEN = "xoxb-944134614374-950594605655-6m0NsYayMl5zg3u9wJbe5HzW"
