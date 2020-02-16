0501d56843aced867540e2af6e4b162a380ceac4
Sida Liu <sliu1@uvm.edu>
2020-02-16 00:55:47 -0500

    receiving msg

--- Proj01/jarvis.py
+++ Proj01/jarvis.py
@@ -1,4 +1,6 @@
+# need do `pip install websocket-client`
 import websocket
+
 try:
     import thread
 except ImportError:
@@ -7,18 +9,19 @@ import time
 import urllib
 import urllib.request
 import json
+
 import botsettings
 
 
 def start_rtm():
     URL = "https://slack.com/api/rtm.connect?token={}".format(
-        botsettings.bot_sida)
-    print(URL)
+        botsettings.bot_sida)  # try change this to bot_sarah
     connection = urllib.request.urlopen(URL)
     text = connection.read().decode("utf-8")
     data = json.loads(text)
     ws_url = data["url"]
     return ws_url
+
 
 def on_message(ws, message):
     try:
@@ -27,29 +30,22 @@ def on_message(ws, message):
         print("received a broken message:")
         print(message)
         return
-    if "type" in msg_data and msg_data["type"]=="message":
-        print("="*10)
-        print("this is a message:")
-        print(msg_data["text"])
-        print("="*10)
+    if "type" in msg_data:
+        if msg_data["type"] == "message":
+            print("="*10)
+            print("this is a message:")
+            print(msg_data["text"])
+            print("="*10)
+        if msg_data["type"] == "hello":
+            print("I am online now. Go to Slack and send me a direct message!")
 
-    # ws.send("hi")
 
 def on_error(ws, error):
     print(error)
 
+
 def on_close(ws):
     print("### closed ###")
-
-def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
-        ws.close()
-        print("thread terminating...")
-    thread.start_new_thread(run, ())
 
 
 if __name__ == "__main__":
@@ -57,9 +53,7 @@ if __name__ == "__main__":
 
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(ws_url,
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
-    # ws.on_open = on_open
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
     ws.run_forever()
-
