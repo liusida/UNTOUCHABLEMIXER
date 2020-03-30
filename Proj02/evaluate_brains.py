9ef6b2024fae1bbac56e15140d538b9965b48900
Sida Liu <sliu1@uvm.edu>
2020-03-29 23:23:09 -0400

    n models x m parameter-pairs comparison. and Voting to combine them.

---
+++ Proj02/evaluate_brains.py
@@ -0,0 +1,110 @@
+# This file evaluates several models and hyper-parameters in sklearn
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import ParameterGrid
+from sklearn.ensemble import VotingClassifier # Voting will help us win the competition! We can combine the top models together!
+
+# Models we will use:
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import NearestCentroid
+from sklearn.neighbors import RadiusNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import RidgeClassifier
+from sklearn.svm import NuSVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import AdaBoostClassifier
+# Models end
+
+import utils
+
+# All the models we are going to use
+candidate_models = [
+    AdaBoostClassifier,
+    SGDClassifier,
+    MultinomialNB,
+    BernoulliNB,
+    DecisionTreeClassifier,
+    ExtraTreeClassifier,
+    ExtraTreesClassifier,
+    KNeighborsClassifier,
+    LinearSVC,
+    LogisticRegression,
+    NearestCentroid,
+    RandomForestClassifier,
+    RidgeClassifier,
+    NuSVC,
+    GradientBoostingClassifier,
+]
+# The parameters that could pass to model
+candidate_parameters = {
+    "SGDClassifier":            {"max_iter": [10, 100], "tol" : [1e-3]}, 
+    "ExtraTreesClassifier":     {"n_estimators": [20, 200]},
+    "LinearSVC":                {"max_iter": [200, 2000], "multi_class": ["crammer_singer"]},
+    "LogisticRegression":       {"solver": ["lbfgs"], "multi_class": ["auto"]},
+    "RandomForestClassifier":   {"n_estimators": [10,100,1000]},
+    "NuSVC":                    {"gamma": ["scale", "auto"]},
+}
+# Build the model
+brains = []
+for this_model in candidate_models:
+    if this_model.__name__ in candidate_parameters:
+        parameter_grid = candidate_parameters[this_model.__name__]
+        parameters = list(ParameterGrid(parameter_grid))
+        for i, parameter in enumerate(parameters):
+            brain = Pipeline([
+                ("vect", CountVectorizer()),
+                ("tfidf", TfidfTransformer()),
+                ("clf", this_model(**parameter)),
+            ])
+            brains.append( (f"{this_model.__name__}__{i}", brain) )
+# Combine those models together
+JarvisBigBrain = VotingClassifier( estimators=brains, voting="soft" )
+
+# Prepare models for testing
+testing_brains = brains.copy()
+testing_brains.append(("JarvisBigBrain", JarvisBigBrain))
+
+# Preprocess training data
+data_x, data_y = utils.preprocess_data()
+
+# Visually check the data
+if False:
+    print(data_x[:10])
+    print("===")
+    print(data_y[:10])
+
+# Train
+print("Evaluation start.")
+print(f"Size of total data: {len(data_y)}.")
+names = []
+cv_score = []
+cv_scores = []
+timers = []
+for brain in testing_brains:
+    utils.start_timer()
+    print(f"Model name: {brain[0]}", flush=True)
+    scores = cross_val_score(brain[1], data_x, data_y, cv=10)
+    print(f"\t validation score: {sum(scores)/len(scores)}")
+    print(f"\t {scores}.")
+    print("")
+    names.append(brain[0])
+    cv_score.append(sum(scores)/len(scores))
+    cv_scores.append(scores)
+    timers.append(utils.stop_timer())
+
+for i in range(len(names)):
+    print(f"[{timers[i]:.2f} s] {names[i]}: {cv_score[i]:.4f}")
--- Proj02/utils.py
+++ Proj02/utils.py
@@ -1,5 +1,78 @@
 # This file contains all the utilities that make life easier
 #
 
+# =======================================================================
+# Important Constant
+#
+# This filename is specified in Sec 5.1 Proj 02
+brain_filename = "jarvis_UNTOUCHABLEMIXER.pkl"
+# This folder name contains all the data files that will feed into the JarvisBrain
+training_folders = ["training_data", "training_data/external-data"]
+# These labels are specified in Sec 7.2 Proj 01
+targets = ["TIME", "PIZZA", "GREET", "WEATHER", "JOKE"]
+#========================================================================
+
+#========================================================================
+# Useful Tools
+# output error
 def error(error_message=""):
     return print('\033[31m ERROR: ' + '\033[0m' + error_message)
+# timer
+import time
+timer = 0
+def start_timer():
+    global timer
+    timer = time.time()
+def stop_timer():
+    return time.time() - timer
+#========================================================================
+
+#========================================================================
+# Reuseable functionalities
+def preprocess_data():
+    import glob, json, os
+    data_x = targets.copy()
+    data_y = targets.copy()
+    for training_folder in training_folders:
+        training_filenames = glob.glob(training_folder+"/*")
+        for filename in training_filenames:
+            if os.path.isdir(filename):
+                # skip sub-folders
+                continue
+            with open(filename, "r", encoding="UTF-8") as f:
+                lines = f.readlines()
+            for line in lines:
+                if line[0]=="#":
+                    # it is a comment line
+                    continue
+                if len(line.strip()) == 0:
+                    # it is an empty line
+                    continue
+                if line[0]=="{":
+                    # it is a json line
+                    try:
+                        j = json.loads(line)
+                        data_x.append(j["TXT"].strip())
+                        data_y.append(j["ACTION"].strip())
+                    except:
+                        error(f"Json loads error. line: {line}.")
+                else:
+                    try:
+                        strings = line.split(",")
+                        if len(strings)==2:
+                            data_x.append(strings[0].strip())
+                            data_y.append(strings[1].strip())
+                        elif len(strings)>2:
+                            sentence = ",".join(strings[:-1])
+                            data_x.append(sentence.strip())
+                            data_y.append(strings[-1].strip())
+                        else:
+                            error(f"Format error. line: {line}.")
+                    except:
+                        error(f"Split by comma error. line: {line}.")
+    # Check validation of the target
+    target_set = set(targets)
+    for i in data_y:
+        if not i in target_set:
+            error(f"Invalid target value: {i}.")
+    return data_x, data_y
