6b62520a02101e7771ab2773ded58901602e2147
Sida Liu <sliu1@uvm.edu>
2020-03-29 23:24:58 -0400

    using hard voting, so no need for probability interface.

--- Proj02/evaluate_brains.py
+++ Proj02/evaluate_brains.py
@@ -3,12 +3,13 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import ParameterGrid
 from sklearn.ensemble import VotingClassifier # Voting will help us win the competition! We can combine the top models together!
 
-# Models we will use:
+# Models we will use: (TODO: Read the documentation and come up with better Classifiers and Parameters)
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import BernoulliNB
@@ -51,11 +52,11 @@ candidate_models = [
 ]
 # The parameters that could pass to model
 candidate_parameters = {
-    "SGDClassifier":            {"max_iter": [10, 100], "tol" : [1e-3]}, 
+    "SGDClassifier":            {"max_iter": [10, 100], "tol" : [1e-3], "loss": ["log", "modified_huber"]}, 
     "ExtraTreesClassifier":     {"n_estimators": [20, 200]},
     "LinearSVC":                {"max_iter": [200, 2000], "multi_class": ["crammer_singer"]},
     "LogisticRegression":       {"solver": ["lbfgs"], "multi_class": ["auto"]},
-    "RandomForestClassifier":   {"n_estimators": [10,100,1000]},
+    "RandomForestClassifier":   {"n_estimators": [10,30]},
     "NuSVC":                    {"gamma": ["scale", "auto"]},
 }
 # Build the model
@@ -63,20 +64,24 @@ brains = []
 for this_model in candidate_models:
     if this_model.__name__ in candidate_parameters:
         parameter_grid = candidate_parameters[this_model.__name__]
-        parameters = list(ParameterGrid(parameter_grid))
+        parameters = list(ParameterGrid(parameter_grid)) # make grid for parameter-pairs
         for i, parameter in enumerate(parameters):
             brain = Pipeline([
                 ("vect", CountVectorizer()),
                 ("tfidf", TfidfTransformer()),
                 ("clf", this_model(**parameter)),
             ])
-            brains.append( (f"{this_model.__name__}__{i}", brain) )
-# Combine those models together
-JarvisBigBrain = VotingClassifier( estimators=brains, voting="soft" )
+            brains.append( (f"{this_model.__name__}-{i}", brain) )
+    else:
+        brain = Pipeline([
+            ("vect", CountVectorizer()),
+            ("tfidf", TfidfTransformer()),
+            ("clf", this_model()),
+        ])
+        brains.append( (f"{this_model.__name__}", brain) )
 
 # Prepare models for testing
 testing_brains = brains.copy()
-testing_brains.append(("JarvisBigBrain", JarvisBigBrain))
 
 # Preprocess training data
 data_x, data_y = utils.preprocess_data()
@@ -87,13 +92,14 @@ if False:
     print("===")
     print(data_y[:10])
 
-# Train
+# Cross evaluate all possible candidates
 print("Evaluation start.")
 print(f"Size of total data: {len(data_y)}.")
 names = []
 cv_score = []
 cv_scores = []
 timers = []
+
 for brain in testing_brains:
     utils.start_timer()
     print(f"Model name: {brain[0]}", flush=True)
@@ -105,6 +111,19 @@ for brain in testing_brains:
     cv_score.append(sum(scores)/len(scores))
     cv_scores.append(scores)
     timers.append(utils.stop_timer())
-
+# Summarize outcomes
+# padding just for making the output looks nicer
+max_padding_length=0
+for name in names:
+    if len(name)>max_padding_length:
+        max_padding_length = len(name)
 for i in range(len(names)):
-    print(f"[{timers[i]:.2f} s] {names[i]}: {cv_score[i]:.4f}")
+    print(f"[{timers[i]:.2f} s] {names[i]}{'.'*(max_padding_length-len(names[i]))}: {cv_score[i]:.4f}")
+
+# Test JarvisBigBrain
+# Combine those models together (TODO: maybe we should just combine TOP 3? )
+JarvisBigBrain = VotingClassifier( estimators=brains, voting="hard" )
+train_x, test_x, train_y, test_y = train_test_split(data_x, data_y)
+JarvisBigBrain.fit(train_x, train_y)
+score = JarvisBigBrain.score(test_x, test_y)
+print(f"JarvisBigBrain validation score: {score}.")
