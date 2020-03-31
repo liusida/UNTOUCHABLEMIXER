fc25729cdb37b3eca17ca12cdf1c4a895cd8f228
sida <sliu1@uvm.edu>
2020-03-30 20:55:34 -0400

    ready for competition 1

--- Proj02/print_wrong_predictions.py
+++ Proj02/print_wrong_predictions.py
@@ -1,5 +1,5 @@
-# This file evaluates several models and hyper-parameters in sklearn
-
+# This file if for manually checking the wrong predictions.
+import copy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
@@ -82,38 +82,48 @@ for this_model in candidate_models:
 
 # Prepare models for testing
 testing_brains = brains.copy()
+JarvisBigBrain = VotingClassifier( estimators=brains, voting="hard" )
+JarvisBigBrain2 = copy.copy(JarvisBigBrain)
 
 # Preprocess training data
-data_x, data_y = utils.preprocess_data()
-
-# Visually check the data
-if False:
-    print(data_x[:10])
-    print("===")
-    print(data_y[:10])
+data_x, data_y, in_filenames = utils.preprocess_data(with_filename=True)
 
 train_x, test_x, train_y, test_y = train_test_split(data_x, data_y)
 
 # Cross evaluate all possible candidates
+import glob
+problematic_file_counts = {}
+allfiles = glob.glob(utils.training_folders[1]+"/*")
 print("Evaluation start.")
 print(f"Size of total data: {len(data_y)}.")
-names = []
-cv_score = []
-cv_scores = []
-timers = []
+JarvisBigBrain.fit(train_x, train_y)
+data_y_hat = JarvisBigBrain.predict(data_x)
+wrong_predictions = (data_y!=data_y_hat)
+for i in range(len(wrong_predictions)):
+    if wrong_predictions[i]:
+        print(f"{data_x[i]}|| true:[{data_y[i]}] => prediction:[{data_y_hat[i]}]")
+        file = in_filenames[i]
+        if file in problematic_file_counts:
+            problematic_file_counts[file] += 1
+        else:
+            problematic_file_counts[file] = 1
 
-for brain in testing_brains:
-    utils.start_timer()
-    print(f"Training Model name: {brain[0]}", flush=True)
-    brain[1].fit(train_x, train_y)
-    print("")
+print("in files:")
+ids = utils.sorted_dict(problematic_file_counts, desc=True)
+for f in ids:
+    print(f)
 
-    test_y_hat = brain[1].predict(test_x)
-    wrong_predictions = (test_y_hat!=test_y)
-    j = 0
-    for i in range(len(wrong_predictions)):
-        if wrong_predictions[i]:
-            print(f"{test_x[i]} true:[{test_y[i]}] => prediction:[{test_y_hat[i]}]")
-            j+=1
-            if j>20:
-                break
+# in this list, we found those buggy files:
+# and we are going to delete them and use the rest of the data to train a brain to win the competition 1!
+#
+# training_data/external-data/176ea1b1-6426-4cd5-1ea4-5cd69371a71f
+# training_data/external-data/8c1745a7-9a6a-5f92-cca7-4147f6be1f72
+# training_data/external-data/d3447490-96fd-35d0-adf2-0806e5214606
+# training_data/external-data/0870e15c-2fcd-81b5-d24b-ace4307bf326
+# training_data/external-data/1fdb8b32-06d5-99e8-12f1-75ffae3b16ec
+# training_data/external-data/1846d424-c17c-6279-23c6-612f48268673
+# training_data/external-data/11ebcd49-428a-1c22-d5fd-b76a19fbeb1d
+# training_data/external-data/8d723104-f773-83c1-3458-a748e9bb17bc
+# training_data/external-data/e3e70682-c209-4cac-629f-6fbed82c07cd
+# training_data/external-data/12e0c8b2-bad6-40fb-1948-8dec4f65d4d9
+# training_data/external-data/a25b59fd-92e8-e269-d12e-cbc40b9475b1
--- Proj02/utils.py
+++ Proj02/utils.py
@@ -8,6 +8,7 @@
 brain_filename = "jarvis_UNTOUCHABLEMIXER.pkl"
 # This folder name contains all the data files that will feed into the JarvisBrain
 training_folders = ["training_data", "training_data/external-data"]
+cleaned_training_folders = [ "training_data/cleaned-data" ]
 # These labels are specified in Sec 7.2 Proj 01
 targets = ["TIME", "PIZZA", "GREET", "WEATHER", "JOKE"]
 
@@ -28,15 +29,31 @@ def start_timer():
     timer = time.time()
 def stop_timer():
     return time.time() - timer
+# sort a dictionary by value
+def sorted_dict(dictionary, desc=False):
+    if desc:
+        return sorted(dictionary.items(), key=lambda x: x[1])[::-1]
+    else:
+        return sorted(dictionary.items(), key=lambda x: x[1])
+
 #========================================================================
 
 #========================================================================
 # Reuseable functionalities
-def preprocess_data():
+def preprocess_data(cleaned_version=True, with_filename=False):
+    '''
+    cleaned_version: True if we use manually cleaned version for competition, False if we use training_folders.
+    with_filename: True if we keep the filename in the list, so that we can figure out which file the data comes from. False by default.
+    '''
     import glob, json, os
     data_x = targets.copy()
     data_y = targets.copy()
-    for training_folder in training_folders:
+    in_filenames = [""]*len(targets) # just padding some empty strings, to make it aligned with data_x and data_y.
+    if cleaned_version:
+        target_folders = cleaned_training_folders
+    else:
+        target_folders = training_folders
+    for training_folder in target_folders:
         training_filenames = glob.glob(training_folder+"/*")
         for filename in training_filenames:
             if os.path.isdir(filename):
@@ -57,6 +74,8 @@ def preprocess_data():
                         j = json.loads(line)
                         data_x.append(j["TXT"].strip())
                         data_y.append(j["ACTION"].strip())
+                        if with_filename:
+                            in_filenames.append(filename)
                     except:
                         error(f"Json loads error. line: {line}.")
                 else:
@@ -65,10 +84,14 @@ def preprocess_data():
                         if len(strings)==2:
                             data_x.append(strings[0].strip())
                             data_y.append(strings[1].strip())
+                            if with_filename:
+                                in_filenames.append(filename)
                         elif len(strings)>2:
                             sentence = ",".join(strings[:-1])
                             data_x.append(sentence.strip())
                             data_y.append(strings[-1].strip())
+                            if with_filename:
+                                in_filenames.append(filename)
                         else:
                             error(f"Format error. line: {line}.")
                     except:
@@ -78,7 +101,10 @@ def preprocess_data():
     for i in data_y:
         if not i in target_set:
             error(f"Invalid target value: {i}.")
-    return data_x, data_y
+    if with_filename:
+        return data_x, data_y, in_filenames
+    else:
+        return data_x, data_y
 
 
 #Function to read in database from Project 1
