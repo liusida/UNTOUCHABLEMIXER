f87d4b128f3153938ecb0c1c7eac1a850d3c9e02
sida <sliu1@uvm.edu>
2020-04-07 21:45:21 -0400

    code for report 2

---
+++ Proj02/report_2.py
@@ -0,0 +1,150 @@
+import glob, shutil, os
+import numpy as np
+import matplotlib.pyplot as plt
+import utils
+from sklearn.model_selection import train_test_split
+
+external_data_files = "training_data/external-data/*"
+cleaned_data_folder = "training_data/cleaned-data"
+
+# pick GOOD training data from external-data folder and copy them to cleaned-data folder
+def step1():
+    print("Step 1 start. Pick GOOD files from external-data to cleaned-data.")
+    # make a folder for cleaned data
+    if os.path.exists(cleaned_data_folder):
+        shutil.rmtree(cleaned_data_folder)
+    os.mkdir(cleaned_data_folder)
+
+    # get a brain
+    brain = utils.build_a_complex_brain()
+    # get all external-data
+    data_x, data_y, in_filenames = utils.preprocess_data(cleaned_version=False, with_filename=True)
+    train_x, test_x, train_y, test_y = train_test_split(data_x, data_y)
+    print(f"Size of total data: {len(data_y)}.")
+    # count data in each file
+    all_file_counts = {}
+    problematic_file_counts = {}
+    for key in in_filenames:
+        if key=="":
+            continue
+        if key in all_file_counts:
+            all_file_counts[key] += 1
+        else:
+            all_file_counts[key] = 1
+        if key not in problematic_file_counts:
+            problematic_file_counts[key] = 0
+    # count problematic data in each file
+    allfiles = glob.glob(external_data_files)
+    brain.fit(train_x, train_y)
+    data_y_hat = brain.predict(data_x)
+    wrong_predictions = (data_y!=data_y_hat)
+    for i in range(len(wrong_predictions)):
+        if wrong_predictions[i]:
+            # print(f"{data_x[i]}|| true:[{data_y[i]}] => prediction:[{data_y_hat[i]}]")
+            file = in_filenames[i]
+            problematic_file_counts[file] += 1
+
+    # report the result and copy data to cleaned folder
+    print("Petential errors in files:")
+    ids = utils.sorted_dict(problematic_file_counts, desc=True)
+    prefix = "training_data/external-data/"
+    for filename, wrong_num in ids:
+        short_filename = filename[len(prefix):]
+        percentage = 100 * wrong_num / all_file_counts[filename]
+        should_exclude = False
+        if percentage>30:
+            should_exclude = True
+        print(f"{short_filename}: ({wrong_num}/{all_file_counts[filename]}, {percentage:.1f}%) {'recommend to exclude' if should_exclude else ''}")
+        if not should_exclude:
+            shutil.copyfile(filename, f"{cleaned_data_folder}/{short_filename}")
+
+# hold off test data and gradually adding more training data to the brain
+def step2():
+    print("Step 2 start. Show difference of adding external data.")
+    # get externel-data from cleaned folder
+    proj1_data_x, proj1_data_y = utils.project1_data()
+    data_x, data_y = utils.preprocess_data(cleaned_version=True, with_filename=False)
+    total_data = len(data_x)
+    num_train = int(total_data * 0.8)
+    num_test = total_data - num_train
+    step_size = int(num_train/30)
+    
+    # proj1 data + more and more external data
+    result = []
+    for total_train in range(0,num_train,step_size):
+        scores = []
+        for iteration_count in range(10):
+            train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, random_state=iteration_count, train_size=num_train, test_size=num_test)
+
+            x = proj1_data_x + train_x[:total_train]
+            y = proj1_data_y + train_y[:total_train]
+
+            # get a brain
+            brain = utils.build_a_simple_brain()
+            # train
+            brain.fit(x, y)
+            # accuracy
+            score = brain.score(test_x, test_y)
+            scores.append(score)
+        result.append(scores)
+        print(".", end="", flush=True)
+    print("")
+
+    # only external-data
+    external_scores = []
+    for iteration_count in range(10):
+        train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, random_state=iteration_count, train_size=num_train, test_size=num_test)
+
+        # get a brain
+        brain = utils.build_a_simple_brain()
+        # train
+        brain.fit(train_x, train_y)
+        # accuracy
+        score = brain.score(test_x, test_y)
+        external_scores.append(score)
+
+    result.append(external_scores)
+    plt.figure(figsize=(9,6))
+    bplot = plt.boxplot(result)
+    bplot['boxes'][-1].set_linestyle('--')
+    bplot['boxes'][-1].set_color('red')
+    
+    plt.xlabel("# of External Training Data Added to Jarvis Brain")
+    plt.ylabel("Accuracy Score")
+    xtick_val = list(range(0,len(result),4))
+    xtick_name = np.array(xtick_val) * step_size
+    plt.xticks(xtick_val, xtick_name)
+    plt.grid(color='#EEEEEE')
+    plt.savefig("adding_external_data.png")
+    plt.show()
+
+# pour all data together and do cross-validation
+def step3():
+    print("Step 3 start. Pour all data together and do cross-validation.")
+    proj1_data_x, proj1_data_y = utils.project1_data()
+    data_x, data_y = utils.preprocess_data(cleaned_version=True, with_filename=False)
+    data_x = data_x + proj1_data_x
+    data_y = data_y + proj1_data_y
+    scores = []
+    for iteration_count in range(20):
+        train_x, test_x, train_y, test_y = train_test_split(data_x, data_y, random_state=iteration_count)
+        # get a brain
+        brain = utils.build_a_simple_brain()
+        # train
+        brain.fit(train_x, train_y)
+        # accuracy
+        score = brain.score(test_x, test_y)
+        scores.append(score)
+
+    plt.figure(figsize=(9,6))
+    bplot = plt.boxplot(scores)
+    plt.xlabel("")
+    plt.ylabel("Accuracy Score")
+    plt.grid(color='#EEEEEE')
+    plt.savefig("estimate_real_world_test_score.png")
+    plt.show()
+
+if __name__ == "__main__":
+    step1()
+    step2()
+    step3()
--- Proj02/utils.py
+++ Proj02/utils.py
@@ -7,7 +7,7 @@
 # This filename is specified in Sec 5.1 Proj 02
 brain_filename = "jarvis_UNTOUCHABLEMIXER.pkl"
 # This folder name contains all the data files that will feed into the JarvisBrain
-training_folders = ["training_data", "training_data/external-data"]
+training_folders = ["training_data/external-data"]
 cleaned_training_folders = [ "training_data/cleaned-data" ]
 # These labels are specified in Sec 7.2 Proj 01
 targets = ["TIME", "PIZZA", "GREET", "WEATHER", "JOKE"]
@@ -128,3 +128,96 @@ def project1_data():
         data_x.append(line[0].strip())
         data_y.append(line[1].strip())
     return data_x, data_y
+
+
+def build_a_simple_brain():
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.feature_extraction.text import TfidfTransformer
+    from sklearn.pipeline import Pipeline
+    from sklearn.linear_model import RidgeClassifier
+    brain = Pipeline([
+            ("vect", CountVectorizer()),
+            ("tfidf", TfidfTransformer()),
+            ("clf", RidgeClassifier()),
+        ])
+    return brain
+
+def build_a_complex_brain():
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.feature_extraction.text import TfidfTransformer
+    from sklearn.pipeline import Pipeline
+    from sklearn.model_selection import train_test_split
+    from sklearn.model_selection import cross_val_score
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.model_selection import ParameterGrid
+    from sklearn.ensemble import VotingClassifier # Voting will help us win the competition! We can combine the top models together!
+
+    # Models we will use: (TODO: Read the documentation and come up with better Classifiers and Parameters)
+    from sklearn.naive_bayes import MultinomialNB
+    from sklearn.neural_network import MLPClassifier
+    from sklearn.naive_bayes import BernoulliNB
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.tree import ExtraTreeClassifier
+    from sklearn.ensemble import ExtraTreesClassifier
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.svm import LinearSVC
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.neighbors import NearestCentroid
+    from sklearn.neighbors import RadiusNeighborsClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.linear_model import RidgeClassifier
+    from sklearn.svm import NuSVC
+    from sklearn.gaussian_process import GaussianProcessClassifier
+    from sklearn.ensemble import GradientBoostingClassifier
+    from sklearn.linear_model import SGDClassifier
+    from sklearn.ensemble import AdaBoostClassifier
+    # Models end
+    # All the models we are going to use
+    candidate_models = [
+        AdaBoostClassifier,
+        # SGDClassifier,
+        MultinomialNB,
+        BernoulliNB,
+        DecisionTreeClassifier,
+        ExtraTreeClassifier,
+        ExtraTreesClassifier,
+        KNeighborsClassifier,
+        # LinearSVC,
+        LogisticRegression,
+        NearestCentroid,
+        RandomForestClassifier,
+        RidgeClassifier,
+        # NuSVC,
+        GradientBoostingClassifier,
+    ]
+    # The parameters that could pass to model
+    candidate_parameters = {
+        "SGDClassifier":            {"max_iter": [10, 100], "tol" : [1e-3], "loss": ["log", "modified_huber"]}, 
+        "ExtraTreesClassifier":     {"n_estimators": [20, 200]},
+        "LinearSVC":                {"max_iter": [200, 2000], "multi_class": ["crammer_singer"]},
+        "LogisticRegression":       {"solver": ["lbfgs"], "multi_class": ["auto"]},
+        "RandomForestClassifier":   {"n_estimators": [10,30]},
+        "NuSVC":                    {"gamma": ["scale", "auto"]},
+    }
+    # Build the model
+    brains = []
+    for this_model in candidate_models:
+        if this_model.__name__ in candidate_parameters:
+            parameter_grid = candidate_parameters[this_model.__name__]
+            parameters = list(ParameterGrid(parameter_grid)) # make grid for parameter-pairs
+            for i, parameter in enumerate(parameters):
+                brain = Pipeline([
+                    ("vect", CountVectorizer()),
+                    ("tfidf", TfidfTransformer()),
+                    ("clf", this_model(**parameter)),
+                ])
+                brains.append( (f"{this_model.__name__}-{i}", brain) )
+        else:
+            brain = Pipeline([
+                ("vect", CountVectorizer()),
+                ("tfidf", TfidfTransformer()),
+                ("clf", this_model()),
+            ])
+            brains.append( (f"{this_model.__name__}", brain) )
+    brain = VotingClassifier( estimators=brains, voting="hard" )
+    return brain
